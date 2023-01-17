4821948dab3de6a1c212896c671a5fbe5af281bc
mattun wsl <matsu222212@gmail.com>
2023-01-17 20:02:17 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app>
+    <v-app-bar app color="green" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-title>アプリ</v-app-bar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group>
+          <v-list-item v-for="item in listItems" :key="item.id">
+            <v-list-item-title>{{ item.text }}</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <v-btn v-for=" item in btn " @click="currentNum = item">{{ item }}</v-btn>
+
+      <v-container label="ねこ">
+        {{ currentNum }}
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+export default {
+  name: "App",
+  data: () => ({
+    drawer: false,
+    currentNum: 0,
+    listItems: [
+      {
+        id: 1,
+        text: "foo",
+      },
+      {
+        id: 2,
+        text: "bar",
+      },
+      {
+        id: 3,
+        text: "fizz",
+      },
+      {
+        id: 4,
+        text: "buzz",
+      },
+    ],
+    btn: [...Array<number>(200)].map((_: number, i: number) => i + 1),
+  }),
+};
+</script>
---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,30 @@
+/**
+ * plugins/vuetify.ts
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
+
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+// Composables
+import { createVuetify } from 'vuetify'
+
+const vuetify = createVuetify({
+  ssr: true,
+})
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+  theme: {
+    themes: {
+      light: {
+        colors: {
+          primary: '#1867C0',
+          secondary: '#5CBBF6',
+        },
+      },
+    },
+  },
+})
