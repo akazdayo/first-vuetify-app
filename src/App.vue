8d0dd177ce96fdf031fb602950700461aa147532
akaz <82073147+akazdayo@users.noreply.github.com>
2023-01-17 20:16:15 +0900

    レビューサイト

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,85 @@
 <template>
-  <v-app>
-    <v-app-bar app color="green" dark>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-app-bar-title>アプリ</v-app-bar-title>
-    </v-app-bar>
+  <v-parallax src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg">
+    <div class="d-flex flex-column fill-height justify-center align-center text-white">
+      <h1 class="text-h4 font-weight-thin mb-4">
+        Beef
+      </h1>
+      <h4 class="subheading">
+        Very tasty beef
+      </h4>
+      <v-rating v-model="rating" bg-color="orange-lighten-1" color="white"></v-rating>
+      <v-card class="mx-auto" max-width="425">
+        <v-list lines="two">
+          <v-list-subheader>Review</v-list-subheader>
 
-    <v-navigation-drawer v-model="drawer" absolute temporary>
-      <v-list nav dense>
-        <v-list-item-group>
-          <v-list-item v-for="item in listItems" :key="item.id">
-            <v-list-item-title>{{ item.text }}</v-list-item-title>
+          <v-list-item prepend-avatar="https://cdn.vuetifyjs.com/images/lists/1.jpg" title="Very bad beef">
+            <template v-slot:subtitle>
+              I think it is very good beef.The price is cheap and I would like to go back.
+            </template>
           </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-    <v-main>
-      <v-btn v-for=" item in btn " @click="currentNum = item">{{ item }}</v-btn>
 
-      <v-container label="ねこ">
-        {{ currentNum }}
-      </v-container>
-    </v-main>
-  </v-app>
+          <v-divider inset></v-divider>
+
+          <v-list-item prepend-avatar="https://cdn.vuetifyjs.com/images/lists/2.jpg">
+            <template v-slot:title>
+              The above review is fake. <span class="text-grey-lighten-1"></span>
+            </template>
+
+            <template v-slot:subtitle>
+              The above review is fake. Please don't be fooled. This review may also be a fake.
+            </template>
+          </v-list-item>
+
+          <v-divider inset></v-divider>
+
+          <v-list-item prepend-avatar="https://cdn.vuetifyjs.com/images/lists/3.jpg" title="Very good!">
+            <template v-slot:subtitle>
+              I find this pork very tasty.I like it very much.
+            </template>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </div>
+  </v-parallax>
+
+
+  <v-responsive class="overflow-y-auto" max-height="550">
+    <div class="pa-6 text-center position-sticky">
+      Scroll down
+    </div>
+
+    <v-responsive min-height="50vh"></v-responsive>
+
+    <div class="text-center text-body-2 mb-12">
+      The card will appear below:
+    </div>
+
+    <v-lazy v-model="isActive" :options="{
+      threshold: .5
+    }" min-height="200" transition="fade-transition">
+      <v-card class="mx-auto" max-width="336">
+        <v-card-title>Card title</v-card-title>
+
+        <v-card-text>
+          Phasellus magna. Quisque rutrum. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at
+          malesuada velit leo quis pede. Aliquam lobortis. Quisque libero metus, condimentum nec, tempor a, commodo
+          mollis, magna.
+
+          In turpis. In dui magna, posuere eget, vestibulum et, tempor auctor, justo. In turpis. Pellentesque dapibus
+          hendrerit tortor. Ut varius tincidunt libero.
+        </v-card-text>
+      </v-card>
+    </v-lazy>
+  </v-responsive>
+
 </template>
+
 
 <script lang="ts">
 export default {
-  name: "App",
   data: () => ({
-    drawer: false,
-    currentNum: 0,
-    listItems: [
-      {
-        id: 1,
-        text: "foo",
-      },
-      {
-        id: 2,
-        text: "bar",
-      },
-      {
-        id: 3,
-        text: "fizz",
-      },
-      {
-        id: 4,
-        text: "buzz",
-      },
-    ],
-    btn: [...Array<number>(200)].map((_: number, i: number) => i + 1),
+    rating: 1,
+    isActive: false,
   }),
-};
+}
 </script>
